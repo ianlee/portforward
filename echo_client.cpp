086c9d9ef81793393c38e08e2fa180ca86e3a2c5
Luke <ltao@ubuntu>
2014-02-12 00:51:59 -0800

    Implemented Client class and resused code from linux chat program

---
+++ echo_client.cpp
@@ -0,0 +1,63 @@
+#include "echo_client.h"
+
+Client::Client(char * host, int port) : _host(host), _port(port) {}
+
+int Client::create_socket()
+{
+	int sd;
+
+	// Create the socket
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("Cannot create socket");
+	}
+	return sd;
+	
+}
+int Client::connect_to_server(int socket, char * host)
+{
+	struct sockaddr_in server;
+	struct hostent	*hostptr;
+	char **pptr;
+	char str[16];
+
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(_port);
+	if ((hostptr = gethostbyname(host)) == NULL)
+	{
+		fprintf(stderr, "Unknown server address\n");
+		exit(1);
+	}
+	bcopy(hostptr->h_addr, (char *)&server.sin_addr, hostptr->h_length);
+
+	if (connect (socket, (struct sockaddr *)&server, sizeof(server)) == -1)
+	{
+		fprintf(stderr, "Can't connect to server\n");
+		perror("connect");
+		return 0;
+	}
+	printf("Connected:    Server Name: %s\n", hostptr->h_name);
+	pptr = hostptr->h_addr_list;
+	printf("\t\tIP Address: %s\n", inet_ntop(hostptr->h_addrtype, *pptr, str, sizeof(str)));
+	return socket;
+}
+
+int Client::send_msgs(int socket, char * data)
+{
+	return send(socket, data, BUFLEN, 0);
+}
+int Client::recv_msgs(int socket, char * buf)
+{
+	int bytes_read = 0, total_bytes_read = 0;
+	int bytes_to_read = BUFLEN;
+
+	while ((bytes_read = recv (socket, buf, bytes_to_read, 0)) < BUFLEN)
+	{
+		buf += bytes_read;
+		bytes_to_read -= bytes_read;
+		total_bytes_read += bytes_read;
+	}
+
+	return total_bytes_read;
+}
---
+++ echo_client.h
@@ -0,0 +1,44 @@
+#ifndef ECHO_CLIENT_H
+#define ECHO_CLIENT_H
+
+#include <iostream>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <pthread.h>
+
+#define SERVER_TCP_PORT		7000	// Default port
+#define BUFLEN			255  	// Buffer length
+
+class Client {
+
+public:
+	Client(char * host, int port);
+	int run();
+	int create_socket();
+	int connect_to_server(int socket, char * host);
+	int send_msgs(int socket, char * data);
+	int recv_msgs(int socket, char * buf);
+
+private:
+	char * _host;
+	int clientSock, _port;		
+	struct sockaddr_in server;
+	struct hostent	*hostptr;
+	char **pptr;
+	char str[16];
+	pthread_t tid;
+
+
+};
+
+#endif
