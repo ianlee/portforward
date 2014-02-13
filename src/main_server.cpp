80716fd01dbc07f0ee1b53eb5eb6d7649a5cc9c6
ian <ian.sean.lee@gmail.com>
2014-02-13 14:47:15 -0800

    instance...

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 			fprintf(stderr, "Usage: %s [port]\n", argv[0]);
 			exit(1);
 	}
-	MultiThreadServer server(port);
+	MultiThreadServer::Instance();
 	server.run();
 	return 0;
 }
