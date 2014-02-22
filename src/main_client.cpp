c02a4d13d2397c111960f2697dd9923dbb7b8f19
luketao <coolman0893@gmail.com>
2014-02-22 01:27:37 -0800

    Update main_client.cpp

--- src/main_client.cpp
+++ src/main_client.cpp
@@ -12,8 +12,8 @@
 -- PROGRAMMER: Ian Lee, Luke Tao
 --
 -- INTERFACE: int main(int argc, char **argv)
---					   int argc - number of cmd-line arguments
---					   char **argv - double pointer to array of arguments
+--		       int argc - number of cmd-line arguments
+--		       char **argv - double pointer to array of arguments
 --
 -- RETURNS:  0 on success
 --
