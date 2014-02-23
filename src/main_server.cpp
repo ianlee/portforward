7a8c009870845d20996588f9f5f7a7c2f7cd04a2
Ian Lee <ian@autoboxmedia.com>
2014-02-22 20:01:39 -0800

    fixing commandline opts

--- src/main_server.cpp
+++ src/main_server.cpp
@@ -111,7 +111,7 @@ int main(int argc, char **argv)
 ----------------------------------------------------------------------------------------------------------------------*/
 
 void* printThread(void * args){
-	const struct timespec timeout;
+	struct const timespec timeout;
 	timeout.tv_sec=0;
 	timeout.tv_nsec=500000000; // 0.5seconds
 	while(1){
