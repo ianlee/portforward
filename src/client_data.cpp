1eb8e7f420909bdf2c4cacf7d5e84aa2cc510ad9
Ian Lee <ian@autoboxmedia.com>
2014-02-22 21:54:26 -0800

    printing out rtt to stdout

--- src/client_data.cpp
+++ src/client_data.cpp
@@ -258,8 +258,8 @@ int ClientData::setRtt(int socket){
 	std::map<int,client_data>::iterator data = list_of_clients.find(socket);
 	_mutex.unlock();
 	if(data != list_of_clients.end()){
-		std::cout << "found socket for rtt " << std::endl;
-		if(data->second.last_time.tv_sec==0){
+		std::cout << "found socket for rtt " <<data->second.last_time.tv_usec<< std::endl;
+		if(data->second.last_time.tv_usec==0){
 			
 			gettimeofday(&currTime,NULL);
 			//calc rtt
