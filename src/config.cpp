d01a71509913aa71fbba63ca53996ba836ea81a7
luketao <coolman0893@gmail.com>
2014-03-03 16:48:12 -0800

    Added Config Class

---
+++ src/config.cpp
@@ -0,0 +1,38 @@
+#include "config.h"
+
+int Config::parseFile()
+{
+	struct DestData temp;
+
+	while(fscanf(fp, "%d %s %d", &clntPort, destAddr, &destPort))
+	{
+		strncpy(temp.destAddr, destAddr, strlen(destAddr));
+		temp.destPort = destPort;
+		forward_list.insert(std::pair<int, DestData>(clntPort, temp));
+	}
+	fclose(fp);
+	return 0;
+}
+
+void Config::printForwardList()
+{
+	for(std::map<int, DestData>::iterator it = forward_list.begin(); it != forward_list.end(); ++it)
+	{
+		std::cout << "Client Port: " << it->first << "\tServer Dest Host: " << it->second.destAddr;
+		std::cout << "\tServer Dest Port: " << it->second.destPort << std::endl;
+	}
+}
+int Config::setFilename(const char * filename)
+{
+	fp = fopen(filename,"r+");
+	return (fp == NULL) ? -1 : 0;
+}
+
+int main()
+{
+	Config config;
+	config.setFilename("test.txt");
+	config.parseFile();
+	config.printForwardList();
+	return 0;
+}
