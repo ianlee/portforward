bfa0e212596c9a8e151b4af9e50ff59635c288c1
luketao <coolman0893@gmail.com>
2014-03-06 12:56:07 -0800

    New PairSockets class added

---
+++ src/pair_sockets.cpp
@@ -0,0 +1,30 @@
+#include "pair_sockets.h"
+
+void PairSockets::insertPairOfSockets(const int s1, const int s2)
+{
+	socket_list.insert(std::pair<int, int>(s1, s2));
+	socket_list.insert(std::pair<int, int>(s2, s1));
+}
+
+int PairSockets::getSocketFromList(const int socket)
+{
+	std::map<int, int>::iterator it = socket_list.find(socket);
+	return (it != socket_list.end()) ? it->first : -1;
+}
+
+int PairSockets::removeSocketFromList(const int keySocket)
+{
+	std::map<int, int>::iterator it = socket_list.find(keySocket);
+	if(it != socket_list.end())
+	{
+		int destSocket = it->second;
+		socket_list.erase(it);
+		socket_list.erase(destSocket);
+		return 0;
+	}
+	else
+	{
+		std::cerr << "Cannot find and remove socket " << keySocket << std::endl;
+		return -1;
+	}
+}
---
+++ src/pair_sockets.h
@@ -0,0 +1,17 @@
+#ifndef PAIR_SOCKETS_H
+#define PAIR_SOCKETS_H
+
+#include <iostream>
+#include <map>
+
+class PairSockets
+{
+	public:
+		void insertPairOfSockets(const int s1, const int s2);
+		int getSocketFromList(const int socket);
+		int removeSocketFromList(const int keySocket);
+	private:
+		std::map<int, int> socket_list;
+};
+
+#endif
